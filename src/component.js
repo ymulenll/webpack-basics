0697a822ed23d2ad181fc93e64757bc92bde205a
Yoelvis Mulen <ymulenll@gmail.com>
2020-04-28 17:21:08 -0300

    Webpack dev server (#2)
    * add webpack-dev-server configurations

--- src/component.js
+++ src/component.js
@@ -3,10 +3,7 @@ import { join } from "lodash-es";
 export function component() {
   const element = document.createElement("div");
 
-  element.innerHTML = join(
-    ["Hello", "webpack"],
-    " "
-  );
+  element.innerHTML = join(["Hello", "webpack"], " ");
 
   return element;
 }
